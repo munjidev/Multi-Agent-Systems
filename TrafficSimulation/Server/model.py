c52c7f0486b658266686fbd605211a651d9b30ae
munij3 <muniainjuan@icloud.com>
2022-11-28 11:43:24 -0600

    Separated project directories

---
+++ TrafficSimulation/Server/model.py
@@ -0,0 +1,96 @@
+from mesa import Model
+from mesa.time import RandomActivation
+from mesa.space import MultiGrid
+from agent import *
+import json
+
+class RandomModel(Model):
+    """ 
+    Creates a new model with random agents.
+    Args:
+        N: Number of agents in the simulation
+        height, width: The size of the grid to model
+    """
+    def __init__(self, N):
+
+        dataDictionary = json.load(open("mapDictionary.txt"))
+
+        with open('base.txt') as baseFile:
+            lines = baseFile.readlines()
+            self.width = len(lines[0])-1
+            self.height = len(lines)
+
+            self.grid = MultiGrid(self.width, self.height,torus = False) 
+            self.schedule = RandomActivation(self)
+
+            for r, row in enumerate(lines):
+                for c, col in enumerate(row):
+                    if col in ["v", "^", ">", "<"]:
+                        agent = Road_Agent(f"r{r*self.width+c}", self, dataDictionary[col])
+                        self.grid.place_agent(agent, (c, self.height - r - 1))
+                    elif col in ["S", "s"]:
+                        agent = Traffic_Light_Agent(f"tl{r*self.width+c}", self, False if col == "S" else True, int(dataDictionary[col]))
+                        self.grid.place_agent(agent, (c, self.height - r - 1))
+                        self.schedule.add(agent)
+                        # Additionally, add a road agent with same direction as road before traffic light
+
+                    elif col == "#":
+                        agent = Obstacle_Agent(f"ob{r*self.width+c}", self)
+                        self.grid.place_agent(agent, (c, self.height - r - 1))
+                    elif col == "D":
+                        agent = Destination_Agent(f"d{r*self.width+c}", self)
+                        self.grid.place_agent(agent, (c, self.height - r - 1))
+
+        # Generate a graph of the streets
+        self.graph = []
+        for agents, x, y in self.grid.coord_iter():
+            for agent in agents:
+                if isinstance(agent, Road_Agent):
+                    # Check neighbors depending on the direction of the road
+                    print(f"Road: {agent.unique_id} Direction: {agent.direction}")
+                    
+                    # Get the neighbors 
+                    n_ul = self.grid.get_cell_list_contents(agent.pos[0]-1, agent.pos[1]+1)
+                    n_up = self.grid.get_cell_list_contents(agent.pos[0], agent.pos[1]+1)
+                    n_ur = self.grid.get_cell_list_contents(agent.pos[0]+1, agent.pos[1]+1)
+                    n_right = self.grid.get_cell_list_contents(agent.pos[0]+1, agent.pos[1])
+                    n_dl = self.grid.get_cell_list_contents(agent.pos[0]-1, agent.pos[1]-1)
+                    n_dr = self.grid.get_cell_list_contents(agent.pos[0]+1, agent.pos[1]-1)
+                    n_down = self.grid.get_cell_list_contents(agent.pos[0], agent.pos[1]-1)
+                    n_left = self.grid.get_cell_list_contents(agent.pos[0]-1, agent.pos[1])
+
+                    if(agent.direction == "Up"):
+                        neighbors = [n_left, n_ul, n_up, n_ur, n_right]
+                    elif(agent.direction == "Right"):
+                        neighbors = [n_up, n_ur, n_right, n_dr, n_down]
+                    elif(agent.direction == "Down"):
+                        neighbors = [n_right, n_dr, n_down, n_dl, n_left]
+                    elif(agent.direction == "Left"):
+                        neighbors = [n_down, n_dl, n_left, n_ul, n_up]
+                    
+                    # Filter out neighbors that are not roads, or if they are roads pointing towards the current road
+
+                    # CHeck that neighbors are roads, and discard the ones that are not roads, discard roads pointing to this one
+                    for neighbor in neighbors:
+                        if isinstance(neighbor, Road_Agent):
+                            if (neighbor == n_up and agent.direction == "Down") or (neighbor == n_down and agent.direction == "Up") or (neighbor == n_left and agent.direction == "Right") or (neighbor == n_right and agent.direction == "Left"):
+                                neighbors.remove(neighbor)
+                            elif (neighbor == n_ul and (agent.direction == "Down" or agent.direction == "Right")) or (neighbor == n_ur and (agent.direction == "Down" or agent.direction == "Left")) or (neighbor == n_dl and (agent.direction == "Up" or agent.direction == "Right")) or (neighbor == n_dr and (agent.direction == "Up" or agent.direction == "Left")):
+                                neighbors.remove(neighbor)
+                        else:
+                            neighbors.remove(neighbor)
+
+                        
+                    # self.graph[agent.unique_id] = agent.getNeighbours()
+
+        self.num_agents = N
+        self.running = True
+
+    def step(self):
+        '''Advance the model by one step.'''
+        self.schedule.step()
+        if self.schedule.steps % 10 == 0:
+            for agents, x, y in self.grid.coord_iter():
+                for agent in agents:
+                    if isinstance(agent, Traffic_Light_Agent):
+                        agent.state = not agent.state
---
+++ TrafficSimulation/TrafficVisualization/Assets/Scripts/AgentController.cs
@@ -0,0 +1,444 @@
+﻿/*
+TC2008B. Sistemas Multiagentes y Gráficas Computacionales
+AgentController.cs | 2022
+Salvador Federico Milanes Braniff
+Juan Muniain Otero
+Miguel Bustamante Perez
+Manuel Barrera Lopez Portillo
+
+Script to control the agents in the scene.
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[Serializable]
+public class AgentData
+{
+    public string id;
+    public float x, y, z;
+
+    public AgentData(string id, float x, float y, float z)
+    {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+[Serializable]
+public class RobotData
+{
+    public string id;
+    public float x, y, z;
+    public bool has_package;
+
+    public RobotData(string id, bool has_package, float x, float y, float z)
+    {
+        this.id = id;
+        this.has_package = has_package;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+[Serializable]
+public class DepotData
+{
+    public string id;
+    public int package_num;
+    public float x, y, z;
+
+    public DepotData(string id, int package_num, float x, float y, float z)
+    {
+        this.id = id;
+        this.package_num = package_num;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+[Serializable]
+public class PackageData
+{
+    public string id;
+    public bool picked_up;
+    public float x, y, z;
+
+    public PackageData(string id, bool picked_up, float x, float y, float z)
+    {
+        this.id = id;
+        this.picked_up = picked_up;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+[Serializable]
+public class AgentsData
+{
+    public List<AgentData> positions;
+
+    public AgentsData() => this.positions = new List<AgentData>();
+}
+
+[Serializable]
+public class RobotsData
+{
+    public List<RobotData> data;
+
+    public RobotsData() => this.data = new List<RobotData>();
+}
+
+[Serializable]
+public class DepotsData
+{
+    public List<DepotData> data;
+
+    public DepotsData() => this.data = new List<DepotData>();
+}
+
+[Serializable]
+public class PackagesData
+{
+    public List<PackageData> data;
+
+    public PackagesData() => this.data = new List<PackageData>();
+}
+
+
+public class AgentController : MonoBehaviour
+{
+    // private string url = "https://agents.us-south.cf.appdomain.cloud/";
+    string serverUrl = "http://localhost:8585";
+    string getAgentsEndpoint = "/getAgents";
+    string getDepotsEndpoint = "/getDepots";
+    string getPackagesEndpoint = "/getPackages";
+    string getObstaclesEndpoint = "/getObstacles";
+    string sendConfigEndpoint = "/init";
+    string updateEndpoint = "/update";
+
+    AgentsData obstacleData;
+    RobotsData agentsData;
+    DepotsData depotsData;
+    PackagesData packagesData;
+    Dictionary<string, GameObject> agents, depots, packages;
+    Dictionary<string, Vector3> prevPositions, currPositions;
+
+    bool updated = false, agentsStarted = false, depotsStarted = false, packagesStarted = false;
+
+    public GameObject agentPrefab, obstaclePrefab, packagePrefab, depotPrefab, floor;
+    public int NAgents, NPackages, width, height;
+    public float timeToUpdate = 5.0f;
+    private int NDepots;
+    private float timer, dt;
+
+    void Start()
+    {
+        agentsData = new RobotsData();
+        depotsData = new DepotsData();
+        packagesData = new PackagesData();
+        obstacleData = new AgentsData();
+
+        prevPositions = new Dictionary<string, Vector3>();
+        currPositions = new Dictionary<string, Vector3>();
+
+        agents = new Dictionary<string, GameObject>();
+        depots = new Dictionary<string, GameObject>();
+        packages = new Dictionary<string, GameObject>();
+
+        floor.transform.localScale = new Vector3((float)width/10, 1, (float)height/10);
+        floor.transform.localPosition = new Vector3((float)width/2-0.5f, 0, (float)height/2-0.5f);
+        
+        timer = timeToUpdate;
+
+        StartCoroutine(SendConfiguration());
+    }
+
+    private void Update() 
+    {
+        // End the simulation if all the packages have been placed in the depots
+        if (packages.Count == 0)
+        {
+            Debug.Log("SUCCESS!");
+            Debug.Log("All packages have been placed in the depots");
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        
+
+        if(timer < 0)
+        {
+            timer = timeToUpdate;
+            updated = false;
+            StartCoroutine(UpdateSimulation());
+        }
+
+        if (updated)
+        {
+            timer -= Time.deltaTime;
+            dt = 1.0f - (timer / timeToUpdate);
+
+            foreach(var agent in currPositions)
+            {
+                Vector3 currentPosition = agent.Value;
+                Vector3 previousPosition = prevPositions[agent.Key];
+
+                Vector3 interpolated = Vector3.Lerp(previousPosition, currentPosition, dt);
+                Vector3 direction = currentPosition - interpolated;
+
+                agents[agent.Key].transform.localPosition = interpolated;
+                if(direction != Vector3.zero) agents[agent.Key].transform.rotation = Quaternion.LookRotation(direction);
+            }
+
+            foreach (var depot in depots)
+            {
+                //Change visible children for the given depot depending on the number of packages
+
+            }
+
+            // float t = (timer / timeToUpdate);
+            // dt = t * t * ( 3f - 2f*t);
+        }
+    }
+ 
+    IEnumerator UpdateSimulation()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(serverUrl + updateEndpoint);
+        yield return www.SendWebRequest();
+ 
+        if (www.result != UnityWebRequest.Result.Success)
+            Debug.Log(www.error);
+        else 
+        {
+            // These coroutines will update the data of the agents, depots and packages
+            StartCoroutine(GetAgentsData());
+            StartCoroutine(GetDepotsData());
+            StartCoroutine(GetPackagesData());
+        }
+    }
+
+    IEnumerator SendConfiguration()
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("NAgents", NAgents.ToString());
+        form.AddField("NPackages", NPackages.ToString());
+        form.AddField("NDepots", NDepots.ToString());
+        form.AddField("width", width.ToString());
+        form.AddField("height", height.ToString());
+
+        UnityWebRequest www = UnityWebRequest.Post(serverUrl + sendConfigEndpoint, form);
+        www.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Configuration upload complete!");
+            Debug.Log("Getting Agents positions");
+            StartCoroutine(GetAgentsData());
+            Debug.Log("Getting Packages positions");
+            StartCoroutine(GetPackagesData());
+            StartCoroutine(GetObstacleData());
+            StartCoroutine(GetDepotsData());
+        }
+    }
+
+    IEnumerator GetAgentsData()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(serverUrl + getAgentsEndpoint);
+        yield return www.SendWebRequest();
+ 
+        if (www.result != UnityWebRequest.Result.Success)
+            Debug.Log(www.error);
+        else
+        {
+            agentsData = JsonUtility.FromJson<RobotsData>(www.downloadHandler.text);
+
+            // Update the positions of the agents
+            foreach(RobotData agent in agentsData.data)
+            {
+                Vector3 newAgentPosition = new Vector3(agent.x, agent.y, agent.z);
+
+                if(!agentsStarted)
+                {
+                    prevPositions[agent.id] = newAgentPosition;
+                    agents[agent.id] = Instantiate(agentPrefab, newAgentPosition, Quaternion.identity);
+                }
+                else
+                {
+                    Vector3 currentPosition = new Vector3();
+                    if(currPositions.TryGetValue(agent.id, out currentPosition))
+                        prevPositions[agent.id] = currentPosition;
+                    currPositions[agent.id] = newAgentPosition;
+
+                    if(agent.has_package)
+                    {
+                        agents[agent.id].transform.GetChild(0).gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        agents[agent.id].transform.GetChild(0).gameObject.SetActive(false);
+                    }
+                }
+            }
+
+            updated = true;
+            if(!agentsStarted) agentsStarted = true;
+        }
+    }
+
+    IEnumerator GetPackagesData()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(serverUrl + getPackagesEndpoint);
+        yield return www.SendWebRequest();
+ 
+        if (www.result != UnityWebRequest.Result.Success)
+            Debug.Log(www.error);
+        else
+        {
+            packagesData = JsonUtility.FromJson<PackagesData>(www.downloadHandler.text);
+            Debug.Log(www.downloadHandler.text);
+
+            foreach(PackageData package in packagesData.data)
+            {
+
+                if(!packagesStarted)
+                {
+                    Debug.Log("Creating package");
+                    packages[package.id] = Instantiate(packagePrefab, new Vector3(package.x, package.y, package.z), Quaternion.identity);
+                }
+                else
+                {
+                    Debug.Log("Package #" + package.id + " was picked up: " + package.picked_up);
+                    if (package.picked_up)
+                    {
+                        //Hide the prefab instance
+                        packages[package.id].SetActive(false);
+
+                    }
+                    else
+                    {
+                        //Show the prefab instance
+                        packages[package.id].SetActive(true);
+                    }
+                }
+            }
+
+            updated = true;
+            if(!packagesStarted) packagesStarted = true;
+        }
+    }
+
+    IEnumerator GetDepotsData()
+    {
+        Debug.Log("Getting Depots data");
+        UnityWebRequest www = UnityWebRequest.Get(serverUrl + getDepotsEndpoint);
+        yield return www.SendWebRequest();
+ 
+        if (www.result != UnityWebRequest.Result.Success)
+            Debug.Log(www.error);
+        else
+        {
+            
+            depotsData = JsonUtility.FromJson<DepotsData>(www.downloadHandler.text);
+            // Debug.Log(www.downloadHandler.text);
+
+            foreach(DepotData depot in depotsData.data)
+            {
+                if(!depotsStarted)
+                {
+                    Debug.Log("Creating depot");
+                    depots[depot.id] = Instantiate(depotPrefab, new Vector3(depot.x, depot.y, depot.z), Quaternion.identity);
+                }
+                else
+                {
+                    Debug.Log("Depot #" + depot.id + " has " + depot.package_num + " packages");
+                    if (depot.package_num==0)
+                    {
+                        depots[depot.id].transform.GetChild(0).gameObject.SetActive(false);
+                        depots[depot.id].transform.GetChild(1).gameObject.SetActive(false);
+                        depots[depot.id].transform.GetChild(2).gameObject.SetActive(false);
+                        depots[depot.id].transform.GetChild(3).gameObject.SetActive(false);
+                        depots[depot.id].transform.GetChild(4).gameObject.SetActive(false);
+
+                    }
+                    else if (depot.package_num==1)
+                    {
+                        depots[depot.id].transform.GetChild(0).gameObject.SetActive(true);
+                        depots[depot.id].transform.GetChild(1).gameObject.SetActive(false);
+                        depots[depot.id].transform.GetChild(2).gameObject.SetActive(false);
+                        depots[depot.id].transform.GetChild(3).gameObject.SetActive(false);
+                        depots[depot.id].transform.GetChild(4).gameObject.SetActive(false);
+                    }
+                    else if (depot.package_num==2)
+                    {
+                        depots[depot.id].transform.GetChild(0).gameObject.SetActive(true);
+                        depots[depot.id].transform.GetChild(1).gameObject.SetActive(true);
+                        depots[depot.id].transform.GetChild(2).gameObject.SetActive(false);
+                        depots[depot.id].transform.GetChild(3).gameObject.SetActive(false);
+                        depots[depot.id].transform.GetChild(4).gameObject.SetActive(false);
+                    }
+                    else if (depot.package_num==3)
+                    {
+                        depots[depot.id].transform.GetChild(0).gameObject.SetActive(true);
+                        depots[depot.id].transform.GetChild(1).gameObject.SetActive(true);
+                        depots[depot.id].transform.GetChild(2).gameObject.SetActive(true);
+                        depots[depot.id].transform.GetChild(3).gameObject.SetActive(false);
+                        depots[depot.id].transform.GetChild(4).gameObject.SetActive(false);
+                    }
+                    else if (depot.package_num==4)
+                    {
+                        depots[depot.id].transform.GetChild(0).gameObject.SetActive(true);
+                        depots[depot.id].transform.GetChild(1).gameObject.SetActive(true);
+                        depots[depot.id].transform.GetChild(2).gameObject.SetActive(true);
+                        depots[depot.id].transform.GetChild(3).gameObject.SetActive(true);
+                        depots[depot.id].transform.GetChild(4).gameObject.SetActive(false);
+                    }
+                    else if (depot.package_num==5)
+                    {
+                        depots[depot.id].transform.GetChild(0).gameObject.SetActive(true);
+                        depots[depot.id].transform.GetChild(1).gameObject.SetActive(true);
+                        depots[depot.id].transform.GetChild(2).gameObject.SetActive(true);
+                        depots[depot.id].transform.GetChild(3).gameObject.SetActive(true);
+                        depots[depot.id].transform.GetChild(4).gameObject.SetActive(true);
+                    }
+                }
+            }
+
+            updated = true;
+            if(!depotsStarted) depotsStarted = true;
+        }
+    }
+
+    IEnumerator GetObstacleData() 
+    {
+        UnityWebRequest www = UnityWebRequest.Get(serverUrl + getObstaclesEndpoint);
+        yield return www.SendWebRequest();
+ 
+        if (www.result != UnityWebRequest.Result.Success)
+            Debug.Log(www.error);
+        else 
+        {
+            obstacleData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
+
+            Debug.Log(obstacleData.positions);
+
+            foreach(AgentData obstacle in obstacleData.positions)
+            {
+                Instantiate(obstaclePrefab, new Vector3(obstacle.x, obstacle.y, obstacle.z), Quaternion.identity);
+            }
+        }
+    }
+}
