42815055791ce11ef11e151632ff188ea0fbcf01
munij3 <muniainjuan@icloud.com>
2022-11-28 12:30:51 -0600

    Changed unity project directory name

--- TrafficSimulation/Server/model.py
+++ TrafficSimulation/Server/model.py
@@ -17,7 +17,7 @@ class RandomModel(Model):
 
         with open('base.txt') as baseFile:
             lines = baseFile.readlines()
-            self.width = len(lines[0])-1
+            self.width = len(lines[0]) - 1
             self.height = len(lines)
 
             self.grid = MultiGrid(self.width, self.height,torus = False) 
@@ -42,14 +42,14 @@ class RandomModel(Model):
                         self.grid.place_agent(agent, (c, self.height - r - 1))
 
         # Generate a graph of the streets
-        self.graph = []
-        for agents, x, y in self.grid.coord_iter():
+        self.graph = {}    # Generate graph dictionary
+        for agents, x, y in self.grid.coord_iter():   # Iterate through all agents
             for agent in agents:
-                if isinstance(agent, Road_Agent):
+                if isinstance(agent, Road_Agent) or isinstance(agent, Traffic_Light_Agent):
                     # Check neighbors depending on the direction of the road
                     print(f"Road: {agent.unique_id} Direction: {agent.direction}")
                     
-                    # Get the neighbors 
+                    # Get the neighbors by their coordinates
                     n_ul = self.grid.get_cell_list_contents(agent.pos[0]-1, agent.pos[1]+1)
                     n_up = self.grid.get_cell_list_contents(agent.pos[0], agent.pos[1]+1)
                     n_ur = self.grid.get_cell_list_contents(agent.pos[0]+1, agent.pos[1]+1)
@@ -59,20 +59,33 @@ class RandomModel(Model):
                     n_down = self.grid.get_cell_list_contents(agent.pos[0], agent.pos[1]-1)
                     n_left = self.grid.get_cell_list_contents(agent.pos[0]-1, agent.pos[1])
 
-                    if(agent.direction == "Up"):
-                        neighbors = [n_left, n_ul, n_up, n_ur, n_right]
-                    elif(agent.direction == "Right"):
-                        neighbors = [n_up, n_ur, n_right, n_dr, n_down]
-                    elif(agent.direction == "Down"):
-                        neighbors = [n_right, n_dr, n_down, n_dl, n_left]
-                    elif(agent.direction == "Left"):
-                        neighbors = [n_down, n_dl, n_left, n_ul, n_up]
-                    
+                    # Check if coordinate contains a road or a traffic light, and generate their neighbors
+
+                    if isinstance(agent, Road_Agent):
+                        if(agent.direction == "Up"):
+                            neighbors = [n_left, n_ul, n_up, n_ur, n_right]
+                        elif(agent.direction == "Right"):
+                            neighbors = [n_up, n_ur, n_right, n_dr, n_down]
+                        elif(agent.direction == "Down"):
+                            neighbors = [n_right, n_dr, n_down, n_dl, n_left]
+                        elif(agent.direction == "Left"):
+                            neighbors = [n_down, n_dl, n_left, n_ul, n_up]
+                    elif isinstance(agent, Traffic_Light_Agent):
+                        if n_down.direction == "Up":
+                            neighbors = [n_left, n_ul, n_up, n_ur, n_right]
+                        elif n_left.direction == "Right":
+                            neighbors = [n_up, n_ur, n_right, n_dr, n_down]
+                        elif n_up.direction == "Down":
+                            neighbors = [n_right, n_dr, n_down, n_dl, n_left]
+                        elif n_right.direction == "Left":
+                            neighbors = [n_down, n_dl, n_left, n_ul, n_up]
+
                     # Filter out neighbors that are not roads, or if they are roads pointing towards the current road
 
-                    # CHeck that neighbors are roads, and discard the ones that are not roads, discard roads pointing to this one
+                    # Check that neighbors are roads, and discard the ones that are not roads, discard roads pointing to this one
                     for neighbor in neighbors:
                         if isinstance(neighbor, Road_Agent):
+                            # Filter invalid directions
                             if (neighbor == n_up and agent.direction == "Down") or (neighbor == n_down and agent.direction == "Up") or (neighbor == n_left and agent.direction == "Right") or (neighbor == n_right and agent.direction == "Left"):
                                 neighbors.remove(neighbor)
                             elif (neighbor == n_ul and (agent.direction == "Down" or agent.direction == "Right")) or (neighbor == n_ur and (agent.direction == "Down" or agent.direction == "Left")) or (neighbor == n_dl and (agent.direction == "Up" or agent.direction == "Right")) or (neighbor == n_dr and (agent.direction == "Up" or agent.direction == "Left")):
@@ -80,8 +93,10 @@ class RandomModel(Model):
                         else:
                             neighbors.remove(neighbor)
 
-                        
-                    # self.graph[agent.unique_id] = agent.getNeighbours()
+                    # Add the current agent to the dictionary with its id as the key and its neighbors as the value
+                    self.graph[agent.unique_id] = neighbors
+
+        print(self.graph)       
 
         self.num_agents = N
         self.running = True
